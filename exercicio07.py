5cdcbac3cb38b7f253569ed8aee46279bdb6f8c7
raissa soares <raissasoaresandradedasilva@gmail.com>
2025-04-10 16:20:58 -0300

    atualizar o exercicio

--- exercicio07.py
+++ exercicio07.py
@@ -1,6 +1,8 @@
 #ler 5 valores, calcular e escrever a média aritmética desses valores lidos
-media = 0
+soma = 0
 for x in range (6):
     nota = float (input("Digita sua nota:"))
-    media = x+5/5
+    soma=soma+nota
+
+media = soma/5
 print (media)
